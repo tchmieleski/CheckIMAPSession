c44b2c9af2799b60e338fadebda642afe1452368
Troy Chmieleski <tchmieleski@incubate.co>
2014-02-02 11:37:32 -0600

    Added Xcode project

---
+++ CheckIMAPSession/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CheckIMAPSession
+//
+//  Created by Troy Chmieleski on 2/2/14.
+//  Copyright (c) 2014 Troy Chmieleski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
